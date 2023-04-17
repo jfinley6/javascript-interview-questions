56c473dab26dc5285f52ce4d69c28f67d44a505f
johnfinley <johnfinleybremerton@gmail.com>
2023-04-17 14:09:16 -0600

    add easyInclude file

---
+++ Question-2/easyInclude.js
@@ -0,0 +1,53 @@
+const local = {
+  redis: {
+    domain: "localhost",
+    apiToken: "4232",
+  },
+};
+
+const development = {
+  redis: {
+    domain: "redis.dev.env.sketchdeck.com",
+  },
+};
+
+const configMap = new Map([
+  ["local", local],
+  ["dev", development],
+]);
+
+// returns a promise that resolves when the connect to Redis is established
+function connectToRedis(env) {
+  const config = configMap.get(env);
+
+  if (config.redis) {
+    console.log(`Connecting to Redis at ${config.redis.domain}`);
+
+    let auth = config.redis.apiToken;
+    if (!auth) {
+      auth = local.redis.apiToken;
+    }
+    return new Promise((resolve, reject) => {
+      if (!auth) {
+        reject(
+          new Error("Connection to Redis failed - no valid auth token found")
+        );
+      }
+      console.log(`Redis connect with auth token ${auth}`);
+      resolve("We connected!");
+    });
+  } else {
+    return Promise.reject(
+      new Error("Connection to Redis failed - No valid Redis config found")
+    );
+  }
+}
+
+connectToRedis("dev")
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((error) => {
+    console.log("Error", error);
+  });
+console.log("Connecting");
